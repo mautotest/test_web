5331bb43131b5ba1f06ddffa995a144776f819fa
majy <609921831@qq/com>
2021-01-06 23:58:17 +0800

    增加页面

--- src/layouts/BasicLayout/menuConfig.ts
+++ src/layouts/BasicLayout/menuConfig.ts
@@ -1,11 +1,11 @@
 const headerMenuConfig = [];
 
 const asideMenuConfig = [
-  {
-    name: 'Dashboard',
-    path: '/',
-    icon: 'smile',
-  },
+  { name: 'Dashboard', path: '/' },
+  { path: '/pipeline', name: '流水线' },
+  { path: '/jobs', name: '任务列表' },
+  { path: '/env', name: '环境信息' },
+  { path: '/runner', name: '执行机列表' },
 ];
 
 export { headerMenuConfig, asideMenuConfig };
---
+++ src/pages/环境信息/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ActionTable from './components/ActionTable';
+
+export default function () {
+  return (
+    <div className="环境信息-page">
+      {/* action bar table */}
+      <ActionTable />
+    </div>
+  );
+}
